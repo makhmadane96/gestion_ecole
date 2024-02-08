e87c66e302a14535a3830f69847248446c66f7d1
makhmadane96 <makhmadane.lo@jmadsolutions.sn>
2024-02-08 20:53:58 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { StatistiqueComponent } from './statistique/statistique.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    StatistiqueComponent
   ],
   imports: [
     BrowserModule
